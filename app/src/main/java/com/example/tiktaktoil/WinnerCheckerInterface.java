4026b5f479d71596bfa0fa89cf3b54e090477776
NazariiSliusar <nazrii.sliusar@gmail.com>
2022-02-12 11:58:51 +0200

    First commit

---
+++ app/src/main/java/com/example/tiktaktoil/WinnerCheckerInterface.java
@@ -0,0 +1,5 @@
+package com.example.tiktaktoil;
+
+public interface WinnerCheckerInterface {
+    Player checkWinner();
+}
---
+++ app/src/main/java/com/example/tiktaktoil/WinnerCheckerVertical.java
@@ -0,0 +1,29 @@
+package com.example.tiktaktoil;
+
+public class WinnerCheckerVertical implements WinnerCheckerInterface{
+    private Game game;
+    public WinnerCheckerVertical(Game game){
+
+    }
+    @Override
+    public Player checkWinner() {
+        Square[][] field = game.getField();
+        Player currPlayer;
+        Player lastPlayer = null ;
+        for (int i = 0, len = field.length; i < len; i++) {
+            lastPlayer = null;
+            int successCounter = 1 ;
+            for (int j = 0,len2 = field[i].length;j < len2; j++){
+                currPlayer = field[j][i].getPlayer();
+                if (currPlayer == lastPlayer && (currPlayer !=null && lastPlayer != null)) {
+                    successCounter++;
+                    if(successCounter == len2){
+                        return currPlayer;
+                    }
+                }
+                lastPlayer = currPlayer;
+            }
+        }
+        return null;
+    }
+}
